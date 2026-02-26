0d9eccdf0097c2c894a301466e3e58c03fcba82e
kbailey2027 <kbailey2027@jamesrumsey.net>
2026-02-26 09:55:25 -0500

    Push #1
    Wrote main void from "Java: Getting Started," and wrote down some notes.

---
+++ index.java
@@ -0,0 +1,6 @@
+// Java: Getting Started
+public class index {
+    public static void main(String [] args) { // Creating void function "main".
+        System.out.println("Hello World!"); // Printing "Hello World!" to the console.
+    }
+}
