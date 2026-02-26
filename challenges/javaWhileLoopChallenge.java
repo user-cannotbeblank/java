3722e2e8b15b0c8864fa4873f7da2bc94435f478
Kay Schluter <schluterkay@gmail.com>
2026-02-26 14:13:29 -0500

    Push #9
    Completed Java While Loop Challenge

---
+++ challenges/javaWhileLoopChallenge.java
@@ -0,0 +1,23 @@
+package challenges;
+
+/*
+Inside main(), complete the following steps:
+
+- Declare an int variable named i and set it to 1
+- Use a while loop that runs as long as i <= 5
+- Inside the loop, print the value of i
+- Increase i by 1 in each loop
+
+Note: Remember to update the loop variable.
+*/
+
+public class javaWhileLoopChallenge {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
