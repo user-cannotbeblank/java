fc611a3ea9c30d720ee8eb09634c779eab1c5924
Kay Schluter <schluterkay@gmail.com>
2026-02-26 16:54:01 -0500

    Push #14
    Completed Java Class Challenge.

---
+++ classes_challenges/javaClassChallenge.java
@@ -0,0 +1,12 @@
+package classes_challenges;
+
+public class javaClassChallenge {
+    String city = "London"; // Attributes the city variable.
+    int population = 9000000; // Attributes the population variable.
+
+    public static void main(String[] args) {
+        javaClassChallenge myObj = new javaClassChallenge(); // Instantiates the javaClassChallenge class and create myObj object.
+        System.out.println(myObj.city); // Prints the value of the city variable for the myObj object.
+        System.out.println(myObj.population); // Prints the value of the population variable for the myObj object.
+    }
+}
