e01eeb581651ed0908be56d81001a227e9cb5b37
Kay Schluter <schluterkay@gmail.com>
2026-02-26 13:15:39 -0500

    Push #6

---
+++ challenges/javaBooleansChallenge.java
@@ -0,0 +1,13 @@
+package challenges;
+
+public class javaBooleansChallenge {
+    public static void main(String[] args) {
+        // 1. Declare two int variables named age and minAge, and assign them values
+        int age = 16;
+        int minAge = 18;
+        boolean isOldEnough = age >= minAge; // I actually added this because I thought it made it more intuitive
+        
+        // 2. Print the result of the comparison age >= minAge (I instead added the isOldEnough booldean and printed that)
+        System.out.println(isOldEnough);
+    }
+}
