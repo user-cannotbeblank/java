2d0f286ad534fbda0bd6a303e84b07563215578a
Kay Schluter <schluterkay@gmail.com>
2026-02-26 15:21:59 -0500

    Push #10
    Completed Java For Loop Challenge

---
+++ challenges/javaForLoopChallenge.java
@@ -0,0 +1,19 @@
+package challenges;
+
+/*
+Inside main(), complete the following steps:
+- Declare int i (either before the loop or inside the for)
+- Use a for loop, and:
+> Set i = 1
+> Run the loop while i <= 5
+> Increase i using i++
+> Inside the loop, print i
+*/
+
+public class javaForLoopChallenge {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
