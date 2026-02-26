621064be51f0cf8a2cb84b3f09d9f2293169b240
Kay Schluter <schluterkay@gmail.com>
2026-02-26 12:52:41 -0500

    Push #5
    Completed Java Strings Challenge

---
+++ challenges/javaStringsChallenge.java
@@ -0,0 +1,18 @@
+package challenges;
+
+public class javaStringsChallenge {
+    public static void main(String[] args) {
+        // 1. Create a string variable named message and assign it a value.
+        String message = "a value";
+
+        // ** Extra work because I want to try something they didn't have us do
+        String fName = "John";
+        String mName = "Wayne";
+        String lName = "Doe";
+        
+        // 2. Print the message
+        System.out.println(message);
+        // ** Extra
+        System.out.println(fName.concat(mName).concat(lName));
+    }
+}
