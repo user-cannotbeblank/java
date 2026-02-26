f458862fc34f77fccf2d787fdefaddf2ceee8d9d
Kay Schluter <schluterkay@gmail.com>
2026-02-26 13:34:31 -0500

    Push #7
    Completed Java ifElse Statement Challenge

---
+++ challenges/java_ifElseChallenge.java
@@ -0,0 +1,16 @@
+package challenges;
+
+public class java_ifElseChallenge {
+    public static void main(String[] args) {
+        // Write age here
+        int age = 16;
+        // Write an if statement that checks if age >= 18
+        if (age >= 18) {
+            // If true, print "Allowed" inside the if block
+            System.out.println("Allowed");
+        } else { // Write an else statement
+            // Inside the else block, print "Not allowed"
+            System.out.println("Not allowed");
+        } 
+    }
+}
