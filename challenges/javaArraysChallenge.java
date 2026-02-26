132469719e74bddf7e27d374bf86142640a375ec
Kay Schluter <schluterkay@gmail.com>
2026-02-26 15:44:25 -0500

    Push #11
    Complete Java Arrays Challenge

---
+++ challenges/javaArraysChallenge.java
@@ -0,0 +1,18 @@
+package challenges;
+
+/*
+Inside main(), complete the following steps:
+
+- Declare an int array named numbers
+- Store the values 1, 2, 3 in the array (in that order)
+- Print the first element of the array
+
+Note: Array indexes start at 0.
+*/
+
+public class javaArraysChallenge {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        System.out.println(numbers[0]);
+    }
+}
